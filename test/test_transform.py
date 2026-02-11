04104b0e6b81ad9d8ec7f2b91c91e5ebc2b3003f
Liyema Koyana <ikoyana36@gmail.com>
2026-02-11 20:41:22 +0200

    Fix tests and edit files

--- test/test_transform.py
+++ test/test_transform.py
@@ -1,9 +1,15 @@
 import pandas as pd
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
+
 from src.transform import transform_data
+
 
 def test_add_director():
     data = pd.DataFrame({
-        "director": [None, "Makaza Humas"]
+        "director": [None, "Makaza Humas"],
+        "date_added": ["2021-01-01", "2021-02-01"] 
     })
     result = transform_data(data)
     assert result["director"].iloc[0] == "Unknown"
