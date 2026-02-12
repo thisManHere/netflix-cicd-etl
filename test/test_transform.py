195cf81f703f6fb7029d5aa20667629cc85862a0
Liyema Koyana <ikoyana36@gmail.com>
2026-02-12 12:04:10 +0200

    Fixing transform import

--- test/test_transform.py
+++ test/test_transform.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
 
-from src.transform import transform_data
+from transform import transform_data
 
 
 def test_add_director():
